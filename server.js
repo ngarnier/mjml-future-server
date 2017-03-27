133571ca5ca41a0b9871449faa66c5f480deed2e
ngarnier <ngarnier@mailjet.com>
2017-03-27 18:43:43 +0200

    First test

---
+++ server.js
@@ -0,0 +1,38 @@
+var rp = require('request-promise')
+var express = require('express')
+const app = express()
+const giphy_url = "http://api.giphy.com/v1/gifs/random"
+const api_key="dc6zaTOxFJmzC"
+
+const giphy = (keyword) => {
+}
+
+app.get('/search/:keyword', (req, res) => {
+  let tag = req.params.keyword
+  let options = {
+    uri: giphy_url,
+    qs: {
+        api_key: api_key,
+        tag: tag
+    },
+    headers: {
+        'User-Agent': 'mj-giphy'
+    },
+    json: true
+};
+ 
+rp(options)
+    .then(gif => {
+      console.log(`gif found for ${tag}: ${gif.data.image_original_url}`)
+      res.send(gif.data.image_original_url)
+    })
+    .catch(err => {
+        console.log(err)
+    });
+})
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('mj-giphy-server running!')
+})
