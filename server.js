0e20f50727ea6a6da4999e00015b1ebe15804050
ngarnier <ngarnier@mailjet.com>
2017-03-29 16:43:03 +0200

    Make the search a query

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-var rp = require('request-promise')
-var express = require('express')
+const rp = require('request-promise')
+const express = require('express')
 const app = express()
 const giphy_url = "http://api.giphy.com/v1/gifs/random"
-const api_key="dc6zaTOxFJmzC"
+const api_key="dc6zaTOxFJmzC" // To export as env variable
 
-const giphy = (keyword) => {
-}
-
-app.get('/search/:keyword?', (req, res) => {
-  let tag = req.params.keyword ? req.params.keyword : ""
+app.get('/search', (req, res) => {
+  let tag = req.query.s ? req.query.s : ""
   let options = {
     uri: giphy_url,
     qs: {
@@ -17,22 +14,19 @@ app.get('/search/:keyword?', (req, res) => {
         tag: tag
     },
     headers: {
-        'User-Agent': 'mj-giphy'
+        'User-Agent': 'mjml-future-server'
     },
     json: true
 };
  
 rp(options)
     .then(gif => {
-      console.log(`gif found for ${tag}: ${gif.data.image_original_url}`)
       res.redirect(gif.data.image_original_url)
     })
     .catch(err => {
         console.log(err)
     });
 })
-
-
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('mj-giphy-server running!')
